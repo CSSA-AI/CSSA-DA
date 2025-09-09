9301e6c674b86b0bbe2b7bc6fbf2db1e42be38af
Angqi Meng <maq20010929@gmail.com>
2025-09-09 19:40:56 +1000

    typo fix

--- packages/rag_core/reranker/base.py
+++ packages/rag_core/reranker/base.py
@@ -1,10 +1,10 @@
 from packages.rag_core.utils.article import Article
 from abc import ABC, abstractmethod
-
+from typing import Tuple, List
 class BaseReranker(ABC):
     def __init__(self):
         pass
 
     @abstractmethod
-    def rerank(self, query: str, articles: list[set[int, float, Article]]) -> list[Article]:
+    def rerank(self, query: str, articles: List[Tuple[int, float, Article]]) -> List[Article]:
         pass
